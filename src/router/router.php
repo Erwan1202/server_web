488310f857015609ecacbb157e39dee5721b1bf8
Lord Erwan <erwan.a.marechal@gmail.com>
2024-11-27 15:23:11 +0100

    Fix indentation in router.php for improved readability

--- src/router/router.php
+++ src/router/router.php
@@ -1,14 +1,13 @@
 <?php
+    require_once __DIR__ . '/../controllers/mainController.php';
 
-require_once __DIR__ . '/../controllers/mainController.php';
-
-$router = new AltoRouter();
-
-$basePath = isset($_SERVER['BASE_URI']) ? $_SERVER['BASE_URI'] : '';
-$router->setBasePath($basePath);
-
-$router->map('GET', '/', 'MainController#home', 'home');
-$router->map('GET', '/about', 'MainController#about', 'about');
-
-return $router;
+    $router = new AltoRouter();
+    
+    $basePath = isset($_SERVER['BASE_URI']) ? $_SERVER['BASE_URI'] : '';
+    $router->setBasePath($basePath);
+    
+    $router->map('GET', '/', 'MainController#home', 'home');
+    $router->map('GET', '/about', 'MainController#about', 'about');
+    
+    return $router;
 ?>
